74338bba83d74377e2fe3e0a59b48323b7d8e67a
aolamide <aboyejiolamide15@gmail.com>
2021-11-12 00:37:39 +0100

    added page to join or host game

--- client/src/components/NameForm/NameForm.jsx
+++ client/src/components/NameForm/NameForm.jsx
@@ -15,12 +15,12 @@ const NameForm = ({ host, gameCode }) => {
     socket.emit('joinGame', { playerName : name, gameCode });
   }
   return (
-    <div>
-      <form onSubmit={host ? hostGame : joinGame}>
-        <input value={name} onChange={e => setName(e.target.value)} type="text" placeholder="Enter your name" />
-        <button>{host ? 'HOST GAME' : 'JOIN GAME'}</button>
+    <>
+      <form className="flex flex-col justify-center items-center bg-white rounded-md p-4 w-11/12 sm:w-96 shadow-md" onSubmit={host ? hostGame : joinGame}>
+        <input required autoComplete=".." value={name} onChange={e => setName(e.target.value)} type="text" placeholder="Enter your nickname" className="w-full rounded-md bg-transparent border-2 border-gray p-2 text-xl" />
+        <button className="text-white rounded-md p-3 mt-3 font-bold w-full bg-black">{host ? 'HOST GAME' : 'JOIN GAME'}</button>
       </form>
-    </div>
+    </>
   )
 };
 
