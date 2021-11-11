81ffb79ba5c886d0001afa530d3c710c88c45a79
aolamide <aboyejiolamide15@gmail.com>
2021-11-12 00:37:01 +0100

    add admin message alert component

---
+++ client/src/components/Messages/AdminMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AdminMessage = ({ message }) => {
+  return (
+    <p className="text-center mb-2 bg-azureish-white text-deep-koamoru p-2 rounded-md">{message}</p>
+  )
+}
+
+export default AdminMessage;
