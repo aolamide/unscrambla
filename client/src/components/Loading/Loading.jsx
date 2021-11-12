8947a2f6e89c61b37d2448696bb5ba21b9d29077
aolamide <aboyejiolamide15@gmail.com>
2021-11-12 03:37:27 +0100

    add loading screen

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Loading = () => {
-  return <p>Loading...</p>
+  return (
+    <div className="h-screen bg-deep-koamoru flex flex-col justify-center items-center">
+      <h1 className="font-cabin text-6xl text-azureish-white">Unscrambla</h1>
+      <div class="bg-white flex space-x-2 p-3 rounded-full justify-center items-center w-max m-auto mt-8 mb-8">
+        <div class="loader-circle c1"></div>
+        <div class="loader-circle c2"></div>
+        <div class="loader-circle c3"></div>
+      </div>
+    </div> 
+  )
 }
 
 export default Loading;
