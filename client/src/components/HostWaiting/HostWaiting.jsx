830ed49b98d858f3e208cdf9ddbe612435e35e5c
aolamide <aboyejiolamide15@gmail.com>
2021-11-14 01:10:10 +0100

    changel class to classname

--- client/src/components/HostWaiting/HostWaiting.jsx
+++ client/src/components/HostWaiting/HostWaiting.jsx
@@ -9,10 +9,10 @@ const HostWaiting = ({ code }) => {
         <p className="text-center font-bold">Tell the other player to visit {window.location.host} and join with the code </p>
         <p className="bg-purple-navy font-bold p-2 text-center text-5xl tracking-widest text-azureish-white rounded-md mt-8 mb-8">{code}</p>
         <p className="text-center">Waiting for player to join...</p>
-        <div class="bg-white flex space-x-2 p-3 rounded-full justify-center items-center w-max m-auto mt-8 mb-8">
-          <div class="loader-circle c1"></div>
-          <div class="loader-circle c2"></div>
-          <div class="loader-circle c3"></div>
+        <div className="bg-white flex space-x-2 p-3 rounded-full justify-center items-center w-max m-auto mt-8 mb-8">
+          <div className="loader-circle c1"></div>
+          <div className="loader-circle c2"></div>
+          <div className="loader-circle c3"></div>
         </div>
       </div>
       <CopyToClipboard text={code} onCopy={() => setCopied(true)}>
